f2af97e26342ea2bf56b0533ad957dd6a414603c
Michael Donaghy <michael.donaghy@visualdna.com>
2014-07-29 17:08:58 +0100

    an implementation that works then

--- src/main/scala/net/cliché/stackingmonadshigh/KleisliClient.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/KleisliClient.scala
@@ -7,9 +7,10 @@ import scala.language.higherKinds
 
 class KleisliClient(profileService: ProfileService) {
   type R[A] = Reader[ApplicationContext, A]
+  type RT[A] = ReaderT[Future, ApplicationContext, A]
   type ET[F[_], B] = EitherT[F, NetworkError, B]
   
   val k = Kleisli[R, UserName, UserProfile](profileService.getProfile _)
-  k.lift[Future].liftMK[ET]
+  k.mapK[RT, UserProfile](_.lift[Future]).liftMK[ET]
 
 }
