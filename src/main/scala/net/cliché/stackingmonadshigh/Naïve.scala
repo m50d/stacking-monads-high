ee1a97163dd768496faaed14dc90a4d1d1d87c56
Michael Donaghy <michael.donaghy@visualdna.com>
2014-07-29 15:20:59 +0100

    getting the idea

--- src/main/scala/net/cliché/stackingmonadshigh/Naïve.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/Naïve.scala
@@ -4,7 +4,10 @@ import scalaz._
 import scalaz.Scalaz._
 import scalaz.concurrent.Future
 
-class Naïve(profileService: ProfileService) {
-  def profile(username: EitherT[({type L[A]=ReaderT[Future, ApplicationContext, A]})#L, NonEmptyList[NetworkError], UserName]) =
-    null
+class Naïve(profileService: ProfileService) extends ProfileClient {
+  def profile(username: EitherT[({ type L[A] = ReaderT[Future, ApplicationContext, A] })#L, NonEmptyList[NetworkError], UserName]) =
+    username.run.map {
+      case -\/(e) ⇒ -\/(e)
+      case \/-(r) ⇒ profileService.getProfile(r)
+    }
 }
