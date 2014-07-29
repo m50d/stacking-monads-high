d9d7e1ce0e83b3a724bda13ea983d5e1df13821b
Michael Donaghy <michael.donaghy@visualdna.com>
2014-07-29 16:38:02 +0100

    less naive implementation looks ok

--- src/main/scala/net/cliché/stackingmonadshigh/LessNaïve.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/LessNaïve.scala
@@ -1,5 +1,15 @@
 package net.cliché.stackingmonadshigh
 
-class LessNaïve(profileService: ProfileService) {
+import scalaz._
+import scalaz.Scalaz._
+import scalaz.concurrent.Future
 
+class LessNaïve(profileService: ProfileService) extends ProfileClient {
+  type ReaderFuture[A] = ReaderT[Future, ApplicationContext, A]
+
+  def profile(username: EitherT[ReaderFuture, NonEmptyList[NetworkError], UserName]) =
+    for {
+      un ← username
+      profile ← profileService.getProfile(un).lift[Future] |> EitherT.right[ReaderFuture, NonEmptyList[NetworkError], UserProfile]
+    } yield profile
 }
