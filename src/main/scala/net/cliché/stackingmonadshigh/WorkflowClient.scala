1fa86e89c39e81e596c3c81da81e6f658074a984
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-01 16:11:35 +0100

    workflow is no good for now

--- src/main/scala/net/cliché/stackingmonadshigh/WorkflowClient.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/WorkflowClient.scala
@@ -1,12 +1,12 @@
 package net.cliché.stackingmonadshigh
 
-import scala.workflow._
+//import scala.workflow._
 import scalaz._
 import scalaz.Scalaz._
 import scalaz.concurrent.Future
 
-class WorkflowClient {
-  context[({type M[B] = EitherT[({ type L[A] = ReaderT[Future, ApplicationContext, A] })#L, NonEmptyList[NetworkError], B]})#M] {
-    1
-  }
-}
+//class WorkflowClient {
+//  context[({type M[B] = EitherT[({ type L[A] = ReaderT[Future, ApplicationContext, A] })#L, NonEmptyList[NetworkError], B]})#M] {
+//    1
+//  }
+//}
