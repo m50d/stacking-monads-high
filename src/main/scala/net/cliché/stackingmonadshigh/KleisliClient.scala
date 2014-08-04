a2b515a667d444b8f824864d515871db1be3336a
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-04 17:06:35 +0100

    something a bit like this

--- src/main/scala/net/cliché/stackingmonadshigh/KleisliClient.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/KleisliClient.scala
@@ -7,7 +7,7 @@ import scala.language.higherKinds
 
 class KleisliClient(profileService: ProfileService) {
   val k =
-//    profileService.getProfileK.mapK[ReaderTFF, UserProfile](_.lift[Future]).liftMK[EitherTF] >=>
-//      profileService.fetchFavouriteTagsK
-    profileService.calculateScoreK.liftMK[ReaderTF].liftMK[EitherTF]
+    profileService.getProfileK.mapK[ReaderTFF, UserProfile](_.lift[Future]).liftMK[EitherTF] >=>
+      profileService.fetchFavouriteTagsK.mapK[ReaderTFF, List[String]](Kleisli.ask).mapK[EitherTFF, List[String]]() >=>
+      profileService.calculateScoreK.liftMK[ReaderTF].liftMK[EitherTF]
 }
