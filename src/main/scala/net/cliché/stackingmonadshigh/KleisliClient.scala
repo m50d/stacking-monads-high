2c8b789d9e1e6f2beda5b7cd8fee0d9c6898f827
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-04 16:43:51 +0100

    starting to get the idea

--- src/main/scala/net/cliché/stackingmonadshigh/KleisliClient.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/KleisliClient.scala
@@ -6,13 +6,6 @@ import scalaz.concurrent.Future
 import scala.language.higherKinds
 
 class KleisliClient(profileService: ProfileService) {
-  type R[A] = Reader[ApplicationContext, A]
-  type RT[A] = ReaderT[Future, ApplicationContext, A]
-  type ET[F[_], B] = EitherT[F, NetworkError, B]
-  
-  val k = Kleisli[R, UserName, UserProfile](profileService.getProfile _)
-  k.mapK[RT, UserProfile](_.lift[Future]).liftMK[ET]
-  
-//  Hoist()
-
+  val k = Kleisli[ReaderF, UserName, UserProfile](profileService.getProfile _)
+  k.mapK[ReaderTFF, UserProfile](_.lift[Future]).liftMK[EitherTF]
 }
--- src/main/scala/net/cliché/stackingmonadshigh/ProfileService.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/ProfileService.scala
@@ -6,9 +6,17 @@ import scalaz.concurrent.Future
 trait ProfileService {
   def getProfile(username: UserName) = Reader((applicationContext: ApplicationContext) ⇒ applicationContext.getProfile(username))
   
+  val getProfileK: Kleisli[ReaderF, UserName, UserProfile]
+  
   def fetchFavouriteTags(profile: UserProfile): NonEmptyList[NetworkError] \/ List[String]
+  
+  val fetchFavouriteTagsK: Kleisli[EitherF, UserProfile, List[String]] 
   
   def calculateScore(data: List[String]): Future[Double]
   
+  val calculateScoreK: Kleisli[Future, List[String], Double]
+  
   def fetchInferredTags(score: Double): EitherT[Future, NonEmptyList[NetworkError], List[String]]
+  
+  val fetchInferredTagsK: Kleisli[({type L[A] = EitherTF[Future, A]})#L, Double, List[String]]
 }
