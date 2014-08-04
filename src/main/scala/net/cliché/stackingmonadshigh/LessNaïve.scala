5546dea71bf905ce9350f6dc8c64fc686d5ecf84
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-04 15:08:11 +0100

    this is making my head hurt

--- src/main/scala/net/cliché/stackingmonadshigh/LessNaïve.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/LessNaïve.scala
@@ -13,11 +13,14 @@ class LessNaïve(profileService: ProfileService) extends ProfileClient {
       profile ← EitherT.right[ReaderTFF, NonEmptyList[NetworkError], UserProfile](profileService.getProfile(un).lift[Future])
     } yield profile
 
-  def complexCalculation(username: EitherTFF[UserName]) =
+  def complexCalculation(username: EitherTFF[UserName]) = {
+      val t = profileService.fetchInferredTags(0.0).run.map(_.liftReaderT[ApplicationContext].run)
     for {
       un ← username
       profile ← EitherT.right[ReaderTFF, NonEmptyList[NetworkError], UserProfile](profileService.getProfile(un).lift[Future])
       tags ← EitherT(profileService.fetchFavouriteTags(profile).point[ReaderTFF])
       score ← EitherT.right[ReaderTFF, NonEmptyList[NetworkError], Double](profileService.calculateScore(tags).liftReaderT[ApplicationContext])
+//      inferredTags ← EitherT(profileService.fetchInferredTags(score).run.map(_.liftReaderT[ApplicationContext]))
     } yield score
+    }
 }
