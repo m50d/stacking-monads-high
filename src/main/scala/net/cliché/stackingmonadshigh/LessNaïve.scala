cd49e0840fc48177519ce4067daf37981c9c4208
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-04 14:29:13 +0100

    getting the idea. Need to uncopypaste all these type aliases.

--- src/main/scala/net/cliché/stackingmonadshigh/LessNaïve.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/LessNaïve.scala
@@ -3,13 +3,23 @@ package net.cliché.stackingmonadshigh
 import scalaz._
 import scalaz.Scalaz._
 import scalaz.concurrent.Future
+import scala.language.higherKinds
 
 class LessNaïve(profileService: ProfileService) extends ProfileClient {
+  type EitherF[A] = NonEmptyList[NetworkError] \/ A
   type ReaderFuture[A] = ReaderT[Future, ApplicationContext, A]
+  type ReaderTF[F[_], A] = ReaderT[F, ApplicationContext, A]
 
   def profile(username: EitherT[ReaderFuture, NonEmptyList[NetworkError], UserName]) =
     for {
       un ← username
       profile ← EitherT.right[ReaderFuture, NonEmptyList[NetworkError], UserProfile](profileService.getProfile(un).lift[Future])
     } yield profile
+   
+  def complexCalculation(username: EitherT[ReaderFuture, NonEmptyList[NetworkError], UserName]) =
+    for {
+      un ← username
+      profile ← EitherT.right[ReaderFuture, NonEmptyList[NetworkError], UserProfile](profileService.getProfile(un).lift[Future])
+      tags ← MonadTrans[ReaderTF].liftM[EitherF, List[String]](profileService.fetchFavouriteTags(profile))
+    } yield {}
 }
--- src/main/scala/net/cliché/stackingmonadshigh/ProfileService.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/ProfileService.scala
@@ -1,7 +1,14 @@
 package net.cliché.stackingmonadshigh
 
 import scalaz._
+import scalaz.concurrent.Future
 
-class ProfileService {
+trait ProfileService {
   def getProfile(username: UserName) = Reader((applicationContext: ApplicationContext) ⇒ applicationContext.getProfile(username))
+  
+  def fetchFavouriteTags(profile: UserProfile): NonEmptyList[NetworkError] \/ List[String]
+  
+  def calculateScore(data: List[String]): Future[Double]
+  
+  def fetchInferredTags(score: Double): EitherT[Future, NonEmptyList[NetworkError], List[String]]
 }
