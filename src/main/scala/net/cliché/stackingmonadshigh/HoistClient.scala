5bd1aaa5407efeb794ce346af11d402934e965eb
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-04 13:55:23 +0100

    almost there then?

--- src/main/scala/net/cliché/stackingmonadshigh/HoistClient.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/HoistClient.scala
@@ -11,11 +11,18 @@ class HoistClient(profileService: ProfileService) {
   type ReaderTFF[A] = ReaderTF[Future, A]
   type ET[F[_], B] = EitherT[F, NetworkError, B]
   type S[A] = ET[ReaderTFF, A]
-  
+
+  def mangle(username: UserName): Future[Float] = null
+
   def profile(username: UserName) = {
     MonadPartialOrder.transformer[ReaderTFF, ET]: MonadPartialOrder[S, ReaderTFF]
     MonadPartialOrder.transformer[Future, ReaderTF]: MonadPartialOrder[ReaderTFF, Future]
-//    MonadPartialOrder.transform
+    MonadPartialOrder.transitive[S, ReaderTFF, Future]: MonadPartialOrder[S, Future]
+    
+    implicitly[S |>=| ReaderTFF]
+    implicitly[ReaderTFF |>=| Future]
+    //Why doesn't this work?
+//    implicitly[S |>=| Future]
+//    implicitly[S |>=| Future].promote(mangle(username))
   }
-//    implicitly[MonadPartialOrder[S, R]].promote(profileService.getProfile(username))
 }
