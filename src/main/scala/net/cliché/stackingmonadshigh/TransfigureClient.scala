a9a38d06d2415fde3ae17cce920cb4787855f43d
Michael Donaghy <michael.donaghy@visualdna.com>
2014-09-15 11:30:15 +0100

    much of the way there

--- src/main/scala/net/cliché/stackingmonadshigh/TransfigureClient.scala
+++ src/main/scala/net/cliché/stackingmonadshigh/TransfigureClient.scala
@@ -3,25 +3,26 @@ package net.cliché.stackingmonadshigh
 import scalaz._
 import scalaz.Scalaz._
 import scalaz.concurrent.Future
-import scalaz.Transfigure._
-import scalaz.TransfigureTo.syntax._
+import scalaz.transfigure._
+import scalaz.transfigure.TransfigureToSyntax._
 import scala.Predef.identity
 
 class TransfigureClient(profileService: ProfileService) {
   def complexCalculation(username: EitherTFF[UserName]) = {
     val un: ReaderF[Future[EitherF[UserName]]] = username.run.mapK[Id, Future[EitherF[UserName]]](identity)
-//    un.transfigureTo[ReaderF, Future](profileService.getProfile _)
-//    transfigureTo[EitherTFF](profileService.getProfile _).run.run
-//      .map(_.transfigureTo[Future, EitherF](profileService.fetchFavouriteTags _))
-      
-    
-    for {
-      un ← username
-      profile ← EitherT.right[ReaderTFF, NonEmptyList[NetworkError], UserProfile](profileService.getProfile(un).lift[Future])
-      tags ← EitherT(profileService.fetchFavouriteTags(profile).point[ReaderTFF])
-      score ← EitherT.right[ReaderTFF, NonEmptyList[NetworkError], Double](profileService.calculateScore(tags).liftReaderT[ApplicationContext])
-      inferredTags ← EitherT(Kleisli { _: ApplicationContext ⇒ profileService.fetchInferredTags(score).run }: ReaderTFF[NonEmptyList[NetworkError] \/ List[String]])
-    } yield inferredTags
+
+    un
+      .transfigureTo3[ReaderF, Future, EitherF](profileService.getProfile _)
+    //    transfigureTo[EitherTFF](profileService.getProfile _).run.run
+    //      .map(_.transfigureTo[Future, EitherF](profileService.fetchFavouriteTags _))
+
+    //    for {
+    //      un ← username
+    //      profile ← EitherT.right[ReaderTFF, NonEmptyList[NetworkError], UserProfile](profileService.getProfile(un).lift[Future])
+    //      tags ← EitherT(profileService.fetchFavouriteTags(profile).point[ReaderTFF])
+    //      score ← EitherT.right[ReaderTFF, NonEmptyList[NetworkError], Double](profileService.calculateScore(tags).liftReaderT[ApplicationContext])
+    //      inferredTags ← EitherT(Kleisli { _: ApplicationContext ⇒ profileService.fetchInferredTags(score).run }: ReaderTFF[NonEmptyList[NetworkError] \/ List[String]])
+    //    } yield inferredTags
   }
 
 }
